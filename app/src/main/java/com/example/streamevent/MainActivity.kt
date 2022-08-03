71029e48f3521ec6d4a735189fb4740739a9459b
mukuru-bless <blessing.chiura@mukuru.com>
2022-08-03 18:22:15 +0200

    Add bottom sheet onclick

--- app/src/main/java/com/example/streamevent/MainActivity.kt
+++ app/src/main/java/com/example/streamevent/MainActivity.kt
@@ -1,24 +1,45 @@
 package com.example.streamevent
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.streamevent.databinding.ActivityMainBinding
 import com.example.streamevent.viewmodel.EventsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private val eventsViewModel: EventsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        eventsViewModel.eventsLiveData.observe(this) { events ->
-            events.forEach {
-                println("hello")
-                println(it.title)
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.eventsMenuItem -> {
+                    Toast.makeText(baseContext, "Events!!", Toast.LENGTH_LONG).show()
+                    true
+                }
+                R.id.scheduleMenuItem -> {
+                    Toast.makeText(baseContext, "Schedule!!", Toast.LENGTH_LONG).show()
+                    true
+                }
+                else -> {
+                    false
+                }
             }
         }
+
+//        eventsViewModel.eventsLiveData.observe(this) { events ->
+//            events.forEach {
+//                println("hello")
+//                println(it.title)
+//            }
+//        }
+
     }
 }
