ff7938033532fe1f10c098f07159ccb5dcd9f830
mukuru-bless <blessing.chiura@mukuru.com>
2022-08-09 19:31:04 +0200

    more ui tests

--- app/src/androidTest/java/com/example/streamevent/view/EventFragmentTest.kt
+++ app/src/androidTest/java/com/example/streamevent/view/EventFragmentTest.kt
@@ -1,16 +1,23 @@
 package com.example.streamevent.view
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.MediumTest
 import com.example.streamevent.R
 import com.example.streamevent.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers
+import org.hamcrest.TypeSafeMatcher
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -20,6 +27,7 @@ import org.mockito.Mockito.verify
 @MediumTest
 @HiltAndroidTest
 @ExperimentalCoroutinesApi
+//not working
 class EventFragmentTest {
     @get: Rule
     var hiltRule = HiltAndroidRule(this)
@@ -33,18 +41,56 @@ class EventFragmentTest {
     fun shouldTestEventFragment() {
         val navController = mock(NavController::class.java)
 
-//        doNothing().`when`(EventFragment().baseActivity)
-//        `when`(EventFragment().baseActivity).thenReturn(this)
-
         launchFragmentInHiltContainer<EventFragment> {
             Navigation.setViewNavController(requireView(), navController)
         }
 
-//        onView(withId(R.id.scheduleMenuItem)).check(matches(isClickable()))
+        onView(withId(R.id.progressIndicator)).check(matches(isDisplayed()))
+
+        Thread.sleep(5000)
+
+        onView(withId(R.id.eventsMenuItem)).check(matches((isClickable())))
+        onView(withId(R.id.eventsMenuItem)).check(matches((isDisplayed())))
+
+        onView(withId(R.id.scheduleMenuItem)).check(matches((isClickable())))
+        onView(withId(R.id.scheduleMenuItem)).check(matches((isDisplayed())))
+
         onView(withId(R.id.scheduleMenuItem)).perform(click())
 
-        verify(navController).navigate(
-            EventFragmentDirections.actionEventFragmentToScheduleFragment()
+        val bottomNavigationItemView = onView(
+            Matchers.allOf(
+                withId(R.id.scheduleMenuItem), withContentDescription("Schedule"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.bottomNavigationView),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
         )
+//        bottomNavigationItemView.perform(click())
+        onView(withId(R.id.scheduleMenuItem)).perform(click())
+
+        Thread.sleep(5000)
+
+        verify(navController).navigate(
+            ScheduleFragmentDirections.actionScheduleFragmentToEventFragment()
+        )
+    }
+
+    private fun childAtPosition(parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent) && view == parent.getChildAt(position)
+            }
+        }
     }
 }
--- app/src/androidTest/java/com/example/streamevent/view/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/streamevent/view/MainActivityTest.kt
@@ -5,10 +5,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnit4
 import com.example.streamevent.R
 import org.hamcrest.Description
 import org.hamcrest.Matcher
@@ -20,22 +21,30 @@ import org.junit.runner.RunWith
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
+//not working
 class MainActivityTest {
 
     @Rule
     @JvmField
-    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun mainActivityTest() {
+        val frameLayout = onView(
+            allOf(
+                withId(R.id.scheduleMenuItem), withContentDescription("Schedule"),
+                withParent(withParent(withId(R.id.bottomNavigationView))),
+                isDisplayed()
+            )
+        )
+        frameLayout.check(matches(isDisplayed()))
+
         val bottomNavigationItemView = onView(
             allOf(
                 withId(R.id.scheduleMenuItem), withContentDescription("Schedule"),
                 childAtPosition(
                     childAtPosition(
-                        withId(R.id.bottomNavigationView),
-                        0
-                    ),
+                        withId(R.id.bottomNavigationView), 0),
                     1
                 ),
                 isDisplayed()
